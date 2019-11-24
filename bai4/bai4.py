ec3004b48778cc32d6adeed430b6dc34c539d475
nguyenhuuthihieu <nguyenhuuthihieu@gmail.com>
2019-11-24 22:13:11 +0700

    bai4 add fourier

--- bai4/bai4.py
+++ bai4/bai4.py
@@ -2,15 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 #  Đọc file ảnh
-image = plt.imread('anh.jpeg').astype(float)
+image = plt.imread('image.png').astype(float)
 plt.figure()
-plt.imshow(image, plt.cm.gray)
+plt.imshow(image)
 plt.title('Original image')
 
 #  Sử dụng fft2 trong fftpack để  thực hiện biến đổi Fourier 2 chiều
 from scipy import fftpack
 img_fft = fftpack.fft2(image)
+def plot_spectrum(img_fft):
+    from matplotlib.colors import LogNorm
+    # A logarithmic colormap
+    plt.imshow(np.abs(img_fft), norm=LogNorm(vmin=5))
+    plt.colorbar()
+
 plt.figure()
+plot_spectrum(img_fft)
 plt.title('Fourier transform')
 
 # Sử dụng ifft2 trong scipy.fftpack để khôi phục lại ảnh gốc.
